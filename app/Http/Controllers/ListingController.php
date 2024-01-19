c9a31a224821d3f9668e56f1a2b9f37c4282b25e
Abdul Wahab Achakzai <abdulwahabachakzai@Wahabs-MacBook-Pro.local>
2024-01-19 15:24:27 +0430

    add[feat]: Filtering list by tags.

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,7 +11,8 @@ class ListingController extends Controller
 
     public function index()
     {
-        return view('listings.index', ['listings' => Listing::all()]);
+
+        return view('listings.index', ['listings' => Listing::latest()->filter(request(['tag']))->get()]);
     }
 
     public function show(Listing $listing)
