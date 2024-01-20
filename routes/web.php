dff590e0e8991e51bf637dd2e3031a659c161b98
Abdul Wahab Achakzai <a.wahabachakzai@gmail.com>
2024-01-20 22:59:00 +0430

    add[feat]: User registration and automatically log in after siging up.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
-use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,6 +35,12 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Single Listing
 Route::get('/listings/{listing}',  [ListingController::class, 'show']);
+
+// Show Create/Register form
+Route::get('/register', [UserController::class, 'create']);
+
+// Create new user
+Route::post('/users', [UserController::class, 'store']);
 
 
 
